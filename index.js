7b04044ef3663a334c558e65fe4cf01a6327a370
Jay <jpnappy@gmail.com>
2016-12-14 13:36:58 -0500

    added endpoint to get data sample

--- index.js
+++ index.js
@@ -7,22 +7,27 @@ const PORT = process.env.PORT || 3000,
 
 app.get('/films/:id/reviews', getFilmReviews);
 app.get('/films/:id/recommendations', getFilmRecommendations);
+app.get('/sample-data', getData);
 
 db.open(DB_PATH)
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => console.error(err.stack));
 
 function getFilmReviews(req, res) {
-  // Example
-  db.all(`SELECT * FROM artists LIMIT 10`)
-    .then((data) => {
-      console.log('Data from SQLite:', data);
-      res.send('Not Implemented');
-    })
+  res.send('Not Implemented');
 }
 
 function getFilmRecommendations(req, res) {
   res.send('Not Implemented');
+}
+
+// To sample data; change name of table to see other data
+function getData(req, res) {
+  db.all(`SELECT * FROM artists LIMIT 10`)
+    .then((data) => {
+      console.log('Data from SQLite:', data);
+    })
+  res.send('Data Sample Sent To Console');
 }
 
 module.exports = app;
