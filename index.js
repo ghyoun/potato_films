490d7fad58e5bcdb7594d0d2db71f4d80f03e868
Matt Fritz <mfritzer@gmail.com>
2017-01-13 13:17:51 -0700

    Remove data access examples

--- index.js
+++ index.js
@@ -5,35 +5,6 @@ const sqlite = require('sqlite'),
 
 const { PORT=3000, NODE_ENV='development', DB_PATH='./db/database.db' } = process.env;
 
-// SEQUELIZE ORM EXAMPLE
-const sequelize = new Sequelize('database', '', '', { 
-  dialect: 'sqlite',
-  storage: DB_PATH
-});
-
-const Review = sequelize.define('review', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true
-  },
-  author: Sequelize.STRING,
-  content: Sequelize.STRING,
-  rating: Sequelize.INTEGER,
-  created_at: Sequelize.DATE,
-}, { timestamps: false });
-
-Review.findAll({
-  limit: 1
-}).then((results) => {
-  console.log('[SEQUELIZE]:', results.map((row) => row.dataValues));
-});
-
-// RAW SQLITE EXAMPLE
-sqlite.open(DB_PATH).then(() => {
-  sqlite.all('SELECT * FROM reviews LIMIT 1')
-    .then((results) => console.log('[SQLITE]:', results));
-});
-
 // START SERVER
 Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
